b73e0ed0754d3280e1d82e035b916cb8f7821201
crazyduck <maxikrause@gmail.com>
2024-05-22 22:30:12 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,14 +8,17 @@ PRINTER_LOCATION="Buero"
 set -m
 
 # Start the cupsd foreground  process and put it in the background
+echo "cupsd starting..."
 /usr/sbin/cupsd -f &
 
 # Check if printer already configured
 FILE=/etc/cups/printers.conf
 if [ -f "$FILE" ]; then
-    echo "cupsd starting..."
+    echo "cupsd started..."
 else 
     echo "First run, creating printer"
+    sleep 5
+    echo "waited 5 secs for cupsd to be up and responding"
     lpadmin -p $PRINTER_NAME -D "$PRINTER_DESCRIPTION" -E -L "$PRINTER_LOCATION" -m canonip7200.ppd -v cnijusb:/dev/usb/lp0
     echo "printer created"
 fi 
