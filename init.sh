b043c5616d7fa0853106a2bbb066ed76b2893d08
crazyduck <maxikrause@gmail.com>
2024-05-22 22:11:16 +0200

    Update init.sh - Quotation typos

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ if [ -f "$FILE" ]; then
     echo "cupsd started..."
 else 
     echo "First run, creating printer"
-    lpadmin -p $PRINTER_NAME -D $PRINTER_DESCRIPTION -E -L $PRINTER_LOCATION -m canonip7200.ppd -v cnijusb:/dev/usb/lp0
+    lpadmin -p $PRINTER_NAME -D "$PRINTER_DESCRIPTION" -E -L "$PRINTER_LOCATION" -m canonip7200.ppd -v cnijusb:/dev/usb/lp0
     echo "printer created"
 fi 
 # now we bring the primary process back into the foreground
