bcb1677c534277024a01dbd33dd8099b87177510
crazyduck <maxikrause@gmail.com>
2024-05-22 22:24:35 +0200

    Update init.sh - multiprocess

--- init.sh
+++ init.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-  
-
 # printer config
 PRINTER_NAME=ip7200
 PRINTER_DESCRIPTION="Canon Pixma ip7200"
 PRINTER_LOCATION="Buero"
+
+# turn on bash's job control
+set -m
+
 # Start the cupsd foreground  process and put it in the background
-  
+echo "cupsd starting..."
+/usr/sbin/cupsd -f &
+
 # Check if printer already configured
 FILE=/etc/cups/printers.conf
 if [ -f "$FILE" ]; then
-    echo "cupsd starting..."
-    /usr/sbin/cupsd -f
 else 
     echo "First run, creating printer"
-    echo "starting cupsd in daemon mode for this run"
-    /usr/sbin/cupsd
     lpadmin -p $PRINTER_NAME -D "$PRINTER_DESCRIPTION" -E -L "$PRINTER_LOCATION" -m canonip7200.ppd -v cnijusb:/dev/usb/lp0
     echo "printer created"
 fi 
+
 # now we bring the primary process back into the foreground
 # and leave it there
+fg %1
